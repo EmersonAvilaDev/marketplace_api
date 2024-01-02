3912af5310e1811fc8186d85de1bdd68c6968cd3
EmersonAvilaDev <avilaadsemerson@gmail.com>
2024-01-02 12:46:49 -0300

    add: storeController

---
+++ src/controller/StoreController.ts
@@ -0,0 +1,48 @@
+import { Request, Response } from "express";
+import { prisma } from "../database/prisma";
+
+export const createStore = async (req: Request, res: Response) => {
+  const { name } = req.body;
+  const { userId } = req.params;
+
+  const isUser = await prisma.user.findUnique({
+    where: {
+      id: userId,
+    },
+  });
+
+  if (!isUser) {
+    return res.status(400).json({
+      message: "Usuário não existe",
+    });
+  }
+
+  const store = await prisma.store.create({
+    data: {
+      name,
+      User: {
+        connect: {
+          id: userId,
+        },
+      },
+    },
+  });
+
+  return res.json(store);
+};
+
+export const getAllStore = async (req: Request, res: Response) => {
+  const stores = await prisma.store.findMany({
+    select: {
+      id: true,
+      name: true,
+      User: {
+        select: {
+          name: true
+        }
+      }
+    }
+  });
+
+  return res.json(stores);
+};
--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,19 @@
 import { Router } from "express";
 import { createUser, deleteManyUser } from "./controller/UserController";
 import { createAccess, getAllAccesses } from "./controller/AccessController";
+import { createStore, getAllStore } from "./controller/StoreController";
 
 export const router = Router();
 
+            /* User */
 router.post("/user", createUser);
+router.delete("/delete-users", deleteManyUser);
+
+            /* Access */
 router.post("/access", createAccess);
 router.get("/accesses", getAllAccesses);
-router.delete("/delete-users", deleteManyUser);
+
+            /* Store */
+router.post("/store/:userId", createStore);
+router.get("/stores", getAllStore);
+
